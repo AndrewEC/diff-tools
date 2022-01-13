559edad458f2f9ee3b58d4ae02f8ad8a72080cec
unknown <andrew.e.cumming@gmail.com>
2022-01-13 10:38:10 -0500

    Removed parent property from pathtree since it's not in use.

--- diff/tree/model.py
+++ diff/tree/model.py
@@ -5,18 +5,16 @@ from pathlib import Path
 
 class PathTree:
 
-    def __init__(self, path: Path, size=-1, checksum=None, parent: PathTree = None):
+    def __init__(self, path: Path, size=-1, checksum=None):
         self.path = path
         self.size = path.stat().st_size if path.is_file() and size == -1 else size
         self.children: List[PathTree] = []
-        self.parent = parent
         self.checksum = checksum
 
     def add_child(self, path: Path):
-        self.children.append(PathTree(path, parent=self))
+        self.children.append(PathTree(path))
 
     def add_child_tree(self, path_tree: PathTree):
-        path_tree.parent = self
         self.children.append(path_tree)
 
     def set_children(self, paths: List[Path]):
