6b3914a70746668fbe9d01d6b8ed77e5e77582f8
unknown <andrew.e.cumming@gmail.com>
2024-02-10 13:25:06 -0500

    Moved duplicate checksum code into common function.

--- diff/checksum.py
+++ diff/checksum.py
@@ -6,6 +6,13 @@ from diff.tree import compute_file_checksum, AVAILABLE_HASH_ALGORITHMS, DEFAULT_
 from diff.errors import NotAFileException
 
 
+def _compute_file_hash(path: str, algo: str) -> str:
+    path_to_compute = Path(path)
+    if not path_to_compute.is_file():
+        raise NotAFileException('file', path_to_compute)
+    return compute_file_checksum(path_to_compute, algo)
+
+
 @click.command('calculate')
 @click.argument('path')
 @click.option('--algo', '-a', type=click.Choice(AVAILABLE_HASH_ALGORITHMS), default=DEFAULT_HASH_ALGORITHM, help='The preferred algorithm to hash the file with.')
@@ -15,10 +22,7 @@ def _calculate(path: str, algo: str):
 
     path: The path to the file to compute the hash of. This must be an existing file and not a directory.
     """
-    path_to_compute = Path(path)
-    if not path_to_compute.is_file():
-        raise NotAFileException('file', path_to_compute)
-    file_hash = compute_file_checksum(path_to_compute, algo)
+    file_hash = _compute_file_hash(path, algo)
     print(f'The file hash is: {file_hash}')
 
 
@@ -28,16 +32,13 @@ def _calculate(path: str, algo: str):
 @click.option('--algo', '-a', type=click.Choice(AVAILABLE_HASH_ALGORITHMS), default=DEFAULT_HASH_ALGORITHM, help='The preferred algorithm to hash the file with.')
 def _verify(path: str, hash: str, algo: str):
     """
-    Computes the has of a given file and compares the computed hash to the provided hash for equality.
+    Computes the hash of a given file and compares said computed hash to the provided hash for equality.
 
     path: The path to the file to compute the hash of. This must be an existing file and not a directory.
 
     hash: The hash previously computed to compare against.
     """
-    path_to_compute = Path(path)
-    if not path_to_compute.is_file():
-        raise NotAFileException('file', path_to_compute)
-    file_hash = compute_file_checksum(path_to_compute, algo)
+    file_hash = _compute_file_hash(path, algo)
     if file_hash != hash:
         print('The calculated file has and the existing hash do not match.')
         print('Hashes are:')
